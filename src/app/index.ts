7bd54ef55812846e85fa54d62ffe1adfa910fef7
半透明空杯 <ulrica1994@qq.com>
2016-12-19 20:03:58 +0800

    module12 finished

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,13 @@
 import * as services from './services';
+import { Store } from './store';
 export { App } from './app';
 export { routes } from './routes';
+
+
 
 const mapValueToArray = (obj) => Object.keys(obj).map(key=>obj[key]);
 
 export const providers = [
+    Store,
     ...mapValueToArray(services)
 ]
