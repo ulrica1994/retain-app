d1df30aed7421d95d7f40a34173f5fe67fda578d
半透明空杯 <ulrica1994@qq.com>
2016-12-19 20:25:32 +0800

    module13 finished

--- src/app/container/notes.ts
+++ src/app/container/notes.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NoteService } from '../services';
+import { Store } from '../store';
 
 @Component({
     // moduleId: module.id,
@@ -31,21 +32,25 @@ import { NoteService } from '../services';
     }
     `]
 })
-export class NotesContainer {
+export class NotesContainer implements OnDestroy {
     notes = [];
-    constructor(private noteService: NoteService) {
+    constructor(private noteService: NoteService,private store: Store) {
       this.noteService.getNotes()
-      .subscribe(resp => this.notes = resp.data);
+      .subscribe();
+      this.store.changes
+      .map(data => data.notes)
+      .subscribe(notes => this.notes = notes)
+
+    }
+    ngOnDestroy() {
+      console.log('destroyed!');
     }
     onNoteChecked(note) {
       this.noteService.completeNote(note)
-      .subscribe(note => {
-        const i = this.notes.findIndex(localNote => localNote.id === note.id);
-        this.notes.splice(i,1);
-      })
+      .subscribe()
     }
     onCreateNote(note) {
       this.noteService.createNote(note)
-      .subscribe(note => this.notes.push(note))
+      .subscribe()
     }
  }
